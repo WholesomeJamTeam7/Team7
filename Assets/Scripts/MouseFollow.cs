ced7aa6ce6ad481639cabe1cc030f921c5d059a1
Cas <jessica.witor18@bathspa.ac.uk>
2022-03-19 16:07:34 +0000

    finished mousefollow

--- Assets/Scripts/MouseFollow.cs
+++ Assets/Scripts/MouseFollow.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class MouseFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isClickDrag;
+    public bool useObjectZ;
+    bool hovered;
+    bool selected;
+    float z;
+    public float customZ = 10f;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        z = transform.position.z;
+    }
+    private void OnMouseOver()
+    {
+        hovered = true;
+    }
+    private void OnMouseExit()
+    {
+        hovered = false;
+    }
+    private void OnMouseDown()
+    {
+        if (!isClickDrag)
+        {
+            if (selected)
+            {
+                selected = false;
+            }
+            else
+            {
+                selected = true;
+            }
+        }
+        else
+        {
+            selected = true;
+        }
+    }
+    private void OnMouseUp()
+    {
+        if (isClickDrag)
+        {
+            selected = false;
+        }
+    }
+    private void Update()
+    {
+        Vector3 temp = Input.mousePosition;
+        if (!useObjectZ)
+        {
+            temp.z = customZ;
+        }
+        else
+        {
+            temp.z = z + transform.localScale.z*2;
+        }
+        if (selected)
+        {
+            this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+        }
     }
 }
