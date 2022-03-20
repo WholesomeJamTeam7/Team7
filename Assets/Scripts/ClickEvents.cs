7fe7342dd43af7ab020548c7d4feb777c4fc8d00
Cas <jessica.witor18@bathspa.ac.uk>
2022-03-20 18:08:24 +0000

    set up my assets in the scene and postprocessing, in process of adding chapters to speech

--- Assets/Scripts/ClickEvents.cs
+++ Assets/Scripts/ClickEvents.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 using UnityEngine.Events;
 public class ClickEvents : MonoBehaviour
 {
+    public bool scaleSpriteOnHover;
+    public GameObject spriteToScale;
+    public float scaleMultiplier;
     public UnityEvent onMouseDown;
     public UnityEvent onMouseUp;
     public UnityEvent onMouseEnter;
     public UnityEvent onMouseExit;
+    Vector3 origScale;
+    private void Start()
+    {
+        if (scaleSpriteOnHover)
+        {
+            origScale = spriteToScale.transform.localScale;
+        }
+    }
     private void OnMouseDown()
     {
         onMouseDown.Invoke();
@@ -18,10 +29,18 @@ public class ClickEvents : MonoBehaviour
     }
     private void OnMouseEnter()
     {
+        if (scaleSpriteOnHover)
+        {
+            spriteToScale.transform.localScale = new Vector3(spriteToScale.transform.localScale.x + scaleMultiplier, spriteToScale.transform.localScale.y + scaleMultiplier, spriteToScale.transform.localScale.z + scaleMultiplier);
+        }
         onMouseEnter.Invoke();
     }
     private void OnMouseExit()
     {
+        if (scaleSpriteOnHover)
+        {
+            spriteToScale.transform.localScale = origScale;
+        }
         onMouseExit.Invoke();
     }
 }
